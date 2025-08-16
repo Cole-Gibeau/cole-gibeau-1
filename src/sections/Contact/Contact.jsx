27a6de5dc7918904c6f1b4ac37db3f7c30adeee1
Cole-Gibeau <156457369+Cole-Gibeau@users.noreply.github.com>
2025-08-16 15:54:30 -0500

    Update Contact Info

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ContactStyles.module.css";
 import email from "../../assets/Email.png";
+import phone from "../../assets/Phone.png";
 function Contact() {
   return (
     <section id="contact" className={styles.container}>
@@ -10,6 +11,10 @@ function Contact() {
       <div className={styles.contactItem}>
         <img src={email} alt="Email Icon" className={styles.emailIcon} />
         <h3>colegibeau@hotmail.com</h3>
+      </div>
+      <div className={styles.contactItem}>
+        <img src={phone} alt="Phone Icon" className={styles.emailIcon} />
+        <h3>763-244-4339</h3>
       </div>
     </section>
   );
